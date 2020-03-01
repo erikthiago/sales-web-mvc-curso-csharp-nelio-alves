de49257b7a167eb127746bd86786524f8b316d24
Erik Sousa <erik.sousa@outlook.com>
2020-03-01 20:45:40 -0300

    Simple create form

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
 using SalesWebMVC.Services;
 
 namespace SalesWebMVC.Controllers
@@ -16,6 +17,19 @@ namespace SalesWebMVC.Controllers
         {
             var list = _service.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _service.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -18,5 +18,11 @@ namespace SalesWebMVC.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
